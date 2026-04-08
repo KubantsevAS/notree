d4a2457a7e507763196a039641fa933725e18e90
aleksandr.kubantsev <veng93@yandex.ru>
2026-04-08 23:53:59 +0300

    build: create table for db, setup docker

---
+++ db/init.sql
@@ -0,0 +1,13 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TYPE node_type AS ENUM ('folder', 'note', 'task');
+
+CREATE TABLE nodes (
+    id UUID DEFAULT uuid_generate_v4(),
+    parent_id UUID,
+    user_id UUID,
+    type node_type,
+    title VARCHAR(255),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+)
