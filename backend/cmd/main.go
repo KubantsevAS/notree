d133b3b22f89454d83fd0d44f466fa7c196dfdf4
aleksandr.kubantsev <veng93@yandex.ru>
2026-04-10 00:35:11 +0300

    feat: set server port listening

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
+	"net/http"
+	"os"
 
 	"github.com/KubantsevAS/notree/backend/internal/config"
 	"github.com/KubantsevAS/notree/backend/pkg/logger"
@@ -13,4 +16,13 @@ func main() {
 	log := logger.SetupLogger(cfg.Env)
 
 	log.Info("Starting Notree backend", slog.String("env", cfg.Env))
+
+	port := os.Getenv("SERVER_PORT")
+	address := fmt.Sprintf(":%s", port)
+	server := http.Server{
+		Addr: address,
+	}
+
+	log.Info("Server is listening", slog.String("port", address))
+	server.ListenAndServe()
 }
