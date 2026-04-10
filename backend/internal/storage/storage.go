021fb664fdff93bd2492921123dbc96a9b1c1d11
aleksandr.kubantsev <veng93@yandex.ru>
2026-04-11 00:53:23 +0300

    feat: connect and setup pgx

---
+++ backend/internal/storage/storage.go
@@ -0,0 +1,20 @@
+package storage
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/KubantsevAS/notree/backend/internal/config"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func CreateDbPool(config *config.DBConfig, log *slog.Logger) *pgxpool.Pool {
+	dbpool, err := pgxpool.New(context.Background(), config.DSN())
+	if err != nil {
+		log.Error("Database connection failed")
+		panic(err)
+	}
+	log.Info("Database connected")
+
+	return dbpool
+}
