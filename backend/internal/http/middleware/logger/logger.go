ab574f96b6fc03034dfdc64b4f878233a1a776ca
aleksandr.kubantsev <veng93@yandex.ru>
2026-08-31 01:02:52 +0300

    feat: add logger middleware test

--- backend/internal/http/middleware/logger/logger.go
+++ backend/internal/http/middleware/logger/logger.go
@@ -9,14 +9,12 @@ import (
 )
 
 func New(log *slog.Logger) func(next http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		log := log.With(
-			slog.String("component", "middleware/logger"),
-		)
-		log.Info("Logger middleware enabled")
+	childLog := log.With(slog.String("component", "middleware/logger"))
+	childLog.Info("Logger middleware enabled")
 
-		fn := func(w http.ResponseWriter, r *http.Request) {
-			entry := log.With(
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			entry := childLog.With(
 				slog.String("method", r.Method),
 				slog.String("path", r.URL.Path),
 				slog.String("remote_addr", r.RemoteAddr),
@@ -35,8 +33,6 @@ func New(log *slog.Logger) func(next http.Handler) http.Handler {
 			}()
 
 			next.ServeHTTP(ww, r)
-		}
-
-		return http.HandlerFunc(fn)
+		})
 	}
 }
