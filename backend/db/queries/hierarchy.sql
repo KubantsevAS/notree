8ce333d1c27facaf5eb88d2594098a045018b87f
aleksandr.kubantsev <veng93@yandex.ru>
2026-09-04 16:47:55 +0300

    feat: add get children to hierarchy sqlc

--- backend/db/queries/hierarchy.sql
+++ backend/db/queries/hierarchy.sql
@@ -8,3 +8,10 @@ WHERE node.id = $1
   AND node.user_id = $2
   AND node.deleted_at IS NULL
   AND parent.deleted_at IS NULL;
+
+-- name: GetChildren :many
+SELECT * FROM nodes
+WHERE parent_id = $1 
+  AND user_id = $2 
+  AND deleted_at IS NULL
+ORDER BY sort_order ASC;
